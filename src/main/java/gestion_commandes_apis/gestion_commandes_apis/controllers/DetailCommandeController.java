914bb360f617d12e69ee4d0ab7e29e788185cdc5
ngoiefranssen <ngoiefranssen04@gmail.com>
2025-06-16 10:05:41 -0400

    Resolve

---
+++ src/main/java/gestion_commandes_apis/gestion_commandes_apis/controllers/DetailCommandeController.java
@@ -0,0 +1,10 @@
+package gestion_commandes_apis.gestion_commandes_apis.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/detail-commandes")
+public class DetailCommandeController {
+    
+}
